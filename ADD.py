2f7b5d57066b8f9782efc6a478a74345121b20a6
Jonas Zehnder <azehnder@eduroamstud-130-82-242-198.unisg.ch>
2018-09-10 14:04:47 +0200

    Changed

--- ADD.py
+++ ADD.py
@@ -23,5 +23,61 @@ curses.use_default_colors()
 def Colors():
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_GREEN)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_RED)
-    curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_WHITE)
+    curses.init_pair(3, curses.COLOR_BLUE, curses.COLOR_WHITE)
     curses.init_pair(4, curses.COLOR_RED, curses.COLOR_MAGENTA)
+
+def gameover(a_heigth, a_width, a_stdscr):
+    Game = []
+    G = [round(a_heigth/2), round((a_width/2)-7)]
+    Game.append(G)
+
+    a_stdscr.addstr(Game[0][0],Game[0][1], "GAME OVER", curses.color_pair(3))
+
+import random
+
+from curses import KEY_RIGHT, KEY_LEFT, KEY_UP, KEY_DOWN
+
+def enemy(a_snake, a_snake_comp, a_new_head, a_food, a_stdscr, a_height, a_width):
+    
+    if a_new_head == a_food:
+
+        if 7>len(a_snake)>5:
+        
+            enemylist = []
+
+            randomex= random.randint(2,(a_height-3))
+                
+            randomey= random.randint(2,(a_width-3))
+                
+            enemy1 = [randomex, randomey]
+
+            enemy2 = [(randomex-1),(randomey-1)]
+
+            enemylist.insert(0, enemy1)
+
+            enemylist.insert(0, enemy2)
+
+            while enemylist in a_snake_comp:
+            
+                enemylist = []
+
+                randomex= random.randint(2,(HEIGHT-3))
+                    
+                randomey= random.randint(2,(WIDTH-3))
+                    
+                enemy1 = [randomex, randomey]
+
+                enemy2 = [(randomex-1),(randomey-1)]
+
+                enemylist.insert(0, enemy1)
+
+                enemylist.insert(0, enemy2)
+
+            a_stdscr.addstr(enemylist[0][0], enemylist [0][1], "&", curses.color_pair(3))
+            a_stdscr.addstr(enemylist[1][0], enemylist [1][1], "&", curses.color_pair(3))
+
+
+
+
+
+
---
+++ Layout.py
@@ -0,0 +1,246 @@
+import curses
+
+#from time import sleep
+
+from curses import KEY_RIGHT, KEY_LEFT, KEY_UP, KEY_DOWN
+
+from ADD import speed 
+
+from ADD import Colors
+
+from ADD import enemy
+
+import random
+
+from ADD import gameover
+
+from time import sleep
+
+
+HEIGHT = 20
+
+WIDTH = 60
+
+
+
+curses.initscr()
+
+Colors()
+
+stdscr = curses.newwin(HEIGHT, WIDTH, 0, 0)
+
+stdscr.keypad(True)
+
+curses.noecho()
+
+stdscr.border(0)
+
+curses.curs_set(0)
+
+stdscr.nodelay(True)
+
+
+
+snake = [[4,10], [4,9], [4,8]]
+
+food = [10,20]
+
+
+# attempt to solve problem with 
+snake_comp=[]
+snake_comp.insert(0, snake[len(snake)-1])
+snake_comp.insert(0, snake[1])
+snake_comp.insert(0, snake[0])
+
+direction = KEY_RIGHT
+
+
+
+stdscr.addstr(food[0], food[1], ' ', curses.color_pair(2))
+
+
+
+while(True):
+
+    stdscr.refresh()
+
+
+
+    # step update
+
+    previousDirection = direction
+
+    keyboardInput = stdscr.getch()
+
+
+
+    # previousDirection KEY_RIGHT
+
+    if keyboardInput in [KEY_UP, KEY_DOWN] and previousDirection == KEY_RIGHT:
+
+        direction = keyboardInput
+
+    # previousDirection KEY_LEFT
+
+    if keyboardInput in [KEY_UP, KEY_DOWN] and previousDirection == KEY_LEFT:
+
+        direction = keyboardInput
+
+    # previousDirection KEY_RIGHT
+
+    if keyboardInput in [KEY_RIGHT, KEY_LEFT] and previousDirection == KEY_UP:
+
+        direction = keyboardInput
+
+    # previousDirection KEY_RIGHT
+
+    if keyboardInput in [KEY_RIGHT, KEY_LEFT] and previousDirection == KEY_DOWN:
+
+        direction = keyboardInput
+
+
+
+
+
+    
+    speed(snake)
+
+
+
+    newHead = []
+
+
+
+    if direction == KEY_RIGHT:
+
+        newHeadX = snake[0][0]
+
+        newHeadY = snake[0][1] + 1
+
+        newHead = [newHeadX, newHeadY]
+
+
+
+    if direction == KEY_LEFT:
+
+        newHeadX = snake[0][0]
+
+        newHeadY = snake[0][1] - 1
+
+        newHead = [newHeadX, newHeadY]
+
+
+
+    if direction == KEY_UP:
+
+        newHeadX = snake[0][0] - 1
+
+        newHeadY = snake[0][1]
+
+        newHead = [newHeadX, newHeadY]
+
+
+
+    if direction == KEY_DOWN:
+
+        newHeadX = snake[0][0] + 1
+
+        newHeadY = snake[0][1]
+
+        newHead = [newHeadX, newHeadY]
+
+
+
+    if newHeadY > WIDTH - 2:
+
+        gameover(HEIGHT, WIDTH, stdscr)
+
+        stdscr.refresh()
+
+        sleep(5)
+
+        break
+
+    if newHeadY < 1:
+
+        gameover(HEIGHT, WIDTH, stdscr)
+
+        stdscr.refresh()
+
+        sleep(5)
+
+        break
+
+    if newHeadX < 1:
+
+        gameover(HEIGHT, WIDTH, stdscr)
+
+        stdscr.refresh()
+
+        sleep(5)
+
+        break
+
+    if newHeadX > HEIGHT -2:
+
+        gameover(HEIGHT, WIDTH, stdscr)
+
+        stdscr.refresh()
+
+        sleep(5)
+
+        break
+
+    if snake[0] in snake[1:]:
+
+        gameover(HEIGHT, WIDTH, stdscr)
+
+        stdscr.refresh()
+
+        sleep(5)
+
+        break
+
+    snake_comp.insert(0, newHead)
+
+    enemy(snake, snake_comp, newHead, food, stdscr, HEIGHT, WIDTH)
+
+    #foooooodd
+
+    if  newHead==food:
+
+        while food in snake_comp:  
+
+            RandomX= random.randint(1,(HEIGHT-2))
+            
+            RandomY= random.randint(1,(WIDTH-2))
+            
+            food = [RandomX, RandomY]
+
+            stdscr.addstr(food[0], food[1], ' ', curses.color_pair(2))
+
+    else:
+
+        oldTail = snake.pop()
+
+        snake_comp.pop()
+
+        stdscr.addch(oldTail[0], oldTail[1], ' ')
+
+
+   
+    snake.insert(0,newHead)
+
+    stdscr.addstr(newHead[0], newHead[1], '+', curses.color_pair(1))
+
+
+
+
+
+
+
+curses.endwin()
+
+print("Score: You've eaten", (len(snake)-3), "apples")
+print("You ran out of the game")
+
